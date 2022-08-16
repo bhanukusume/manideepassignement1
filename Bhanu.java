a796214b3b6bf6f1219166fc7943e5806883d9cd
bhanukusume <bhanukusume49@gmail.com>
2022-08-16 21:20:28 +0530

    assignmeent1

---
+++ Bhanu.java
@@ -0,0 +1,23 @@
+package Assignement;
+
+public class Bhanu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    String s = "Bhanu";// create a String
+    
+    int lengthOfString =s.length();//length of the string 
+   
+    for(int i = 0;i<lengthOfString;i++) {
+    	
+    	char currentChar = s.charAt(i);
+    	
+    
+    System.out.println(currentChar);
+    }
+    
+	}
+
+}
+
+
---
+++ Number.java
@@ -0,0 +1,21 @@
+package Assignement;
+
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 4567;
+		
+        String string_number = Integer.toString(number);
+ 
+        
+        for (int i = 0; i < string_number.length(); i++) {
+ 
+            
+            System.out.println(string_number.charAt(i));
+        }
+    }
+		 
+			 
+	            
+	}
